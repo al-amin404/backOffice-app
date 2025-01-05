bda3a914ec0ad6b288b7d08a3d315b80ffff0c87
Al Amin <linkers4me@gmail.com>
2025-01-06 00:05:32 +0600

    added route for staff id-card check

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Staff;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
     return redirect('/backoffice');
+});
+
+Route::get('staff/{staff_id}', function ($staff_id) {
+    $staff = Staff::where('staff_id', $staff_id)->first();
+    return view('staff_verify', compact('staff'));
 });
