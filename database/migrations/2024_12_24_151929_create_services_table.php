ef75b118674e58c58449e3b27de06a16e96421c7
Al Amin <linkers4me@gmail.com>
2025-01-12 00:51:47 +0600

    set default(false) to is_active column

--- database/migrations/2024_12_24_151929_create_services_table.php
+++ database/migrations/2024_12_24_151929_create_services_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('price')->nullable();
             $table->string('category')->nullable();
             $table->string('image')->nullable();
-            $table->boolean('is_active')->default(true);
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
